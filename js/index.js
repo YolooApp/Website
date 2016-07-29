f24a2fa5c6d4f836d5bb2f9523f9f447219f9b4d
krialix <yasinsinan707@gmail.com>
2016-07-29 16:05:38 +0300

    Update website

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ $(function() {
     $(".menubutton").toggleClass("active");
     $("main").toggleClass("move-to-left");
     $(".sidebar-item").toggleClass("active");
+    $(".sidebar").toggleClass("blue");
   }
 
   $(".menubutton").on("click tap", function() {
@@ -29,5 +30,69 @@ $(function() {
     }
   });
 
+  var menu       = $(".menu"),
+      menuLinks  = $(".menu ul li a"),
+      toggle     = $(".nav-toggle"),
+      toggleIcon = $(".nav-toggle span");
 
+  function toggleThatNav() {
+    if (menu.hasClass("show-nav")) {
+      if (!Modernizr.csstransforms) {
+        menu.removeClass("show-nav");
+        toggle.removeClass("show-nav");
+        menu.animate({
+          right: "-=300"
+        }, 500);
+        toggle.animate({
+          right: "-=300"
+        }, 500);
+      } else {
+        menu.removeClass("show-nav");
+        toggle.removeClass("show-nav");
+      }
+
+    } else {
+      if (!Modernizr.csstransforms) {
+        menu.addClass("show-nav");
+        toggle.addClass("show-nav");
+        menu.css("right", "0px");
+        toggle.css("right", "330px");
+      } else {
+        menu.addClass("show-nav");
+        toggle.addClass("show-nav");
+      }
+    }
+  }
+
+  function changeToggleClass() {
+    toggleIcon.toggleClass("fa-times");
+    toggleIcon.toggleClass("fa-bars");
+  }
+
+  $(function() {
+    toggle.on("click", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      toggleThatNav();
+      changeToggleClass();
+    });
+      // Keyboard Esc event support
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) {
+        if (menu.hasClass("show-nav")) {
+          if (!Modernizr.csstransforms) {
+            menu.removeClass("show-nav");
+            toggle.removeClass("show-nav");
+            menu.css("right", "-300px");
+            toggle.css("right", "30px");
+            changeToggleClass();
+          } else {
+            menu.removeClass("show-nav");
+            toggle.removeClass("show-nav");
+            changeToggleClass();
+          }
+        }
+      }
+    });
+  });
 });
